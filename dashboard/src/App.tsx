226126b4150663227d309d58c49199e0fdb2a1f4
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2025-11-12 19:13:29 +0000

    despliegue

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,11 +1,10 @@
 import { Grid } from '@mui/material';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
       <Grid container spacing={5} justifyContent="center" alignItems="center">
