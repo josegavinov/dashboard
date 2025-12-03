ccceba4cf5b23a5bc3c73d9a246635cc1373561d
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2025-12-03 19:43:27 +0000

    fin guia 15 desplegado

--- dashboard/src/functions/useFetchData.tsx
+++ dashboard/src/functions/useFetchData.tsx
@@ -1,7 +1,7 @@
 import { useEffect,useState } from "react";
 import { type OpenMeteoResponse } from "../types/DashboardTypes";
 
-export default function useFetchData(): OpenMeteoResponse{ 
+export default function useFetchData(): OpenMeteoResponse | undefined{ 
     const URL  = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m&timezone=America%2FChicago';
     const [data, setData] = useState<OpenMeteoResponse>();
     useEffect(() => {
