7ea6b60711e8e6a5358106c84cf5dfb43b22fb0b
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2025-12-17 15:56:28 +0000

    correcion para el deploy

--- dashboard/src/components/ChartUI.tsx
+++ dashboard/src/components/ChartUI.tsx
@@ -4,9 +4,6 @@ import useFetchData from '../functions/useFetchData';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
-const arrValues1 = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const arrValues2 = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const arrLabels = ['A','B','C','D','E','F','G'];
 
 
 export default function ChartUI() {
